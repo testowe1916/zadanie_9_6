1233f6306ee62ff87a42c108fe0f8217040fae1b
Mariusz Sioch <mariusz.sioch@o2.pl>
2017-12-24 10:38:59 +0100

    Mod js

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ var list = document.getElementById('list');
 var add = document.getElementById('addElem');
 add.addEventListener('click', function() {
 	var element = document.createElement('li');
-	element.innerHTML = 'item';
+	var test = document.getElementsByTagName('li').length;
+	element.innerHTML = 'item' +' '+ test;
 	list.appendChild(element);
 });
